7c64db02518772e753c03944b4311a7cc8adc38d
JiaMingLin <robin890650@gmail.com>
2020-05-27 17:50:57 +0800

    correct bitstream path

--- make_all.sh
+++ make_all.sh
@@ -13,3 +13,4 @@ HLS_ROOT=$ROOT/$1
 
 cp $HLS_ROOT/vivado_design/vivado_design.srcs/sources_1/bd/design_1/hw_handoff/design_1.hwh $HLS_ROOT/vivado_design
 cp $HLS_ROOT/vivado_design/vivado_design.runs/impl_1/design_1_wrapper.bit $HLS_ROOT/vivado_design/design_1.bit
+
