cfa8b90ae2082f9d0d06e0f63ce38c48036a9a89
JiaMingLin <robin890650@gmail.com>
2020-05-27 17:48:31 +0800

    correct bitstream path

--- make_all.sh
+++ make_all.sh
@@ -11,5 +11,5 @@ HLS_ROOT=$ROOT/$1
 ./make_ip.sh $HLS_ROOT
 ./make_bitstream.sh $HLS_ROOT
 
-cp $HLS_ROOT/vivado_design/vivado_design.srcs/sources_1/bd/design_1/hw_handoff/design_1.hwh ../output/vivado_design/
-cp $HLS_ROOT/vivado_design/vivado_design.runs/impl_1/design_1_wrapper.bit ../output/vivado_design/design_1.bit
+cp $HLS_ROOT/vivado_design/vivado_design.srcs/sources_1/bd/design_1/hw_handoff/design_1.hwh $HLS_ROOT/vivado_design
+cp $HLS_ROOT/vivado_design/vivado_design.runs/impl_1/design_1_wrapper.bit $HLS_ROOT/vivado_design/design_1.bit
