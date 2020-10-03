b30a5660d74b8bf00bb78cc3ff2a08999e7dff75
JiaMingLin <robin890650@gmail.com>
2020-10-03 18:07:33 +0800

    add zcu102 design

--- make_all.sh
+++ make_all.sh
@@ -10,7 +10,7 @@ HLS_ROOT=$ROOT/$1
 SCALE=$2
 
 ./make_ip.sh $HLS_ROOT $SCALE
-#./make_bitstream.sh $HLS_ROOT $SCALE
+./make_bitstream.sh $HLS_ROOT $SCALE
 
-#cp $HLS_ROOT/vivado_design_$SCALE/vivado_design.srcs/sources_1/bd/design_1/hw_handoff/design_1.hwh $HLS_ROOT/vivado_design_$SCALE
-#cp $HLS_ROOT/vivado_design_$SCALE/vivado_design.runs/impl_1/design_1_wrapper.bit $HLS_ROOT/vivado_design_$SCALE/design_1.bit
+cp $HLS_ROOT/vivado_design_$SCALE/vivado_design.srcs/sources_1/bd/design_1/hw_handoff/design_1.hwh $HLS_ROOT/vivado_design_$SCALE
+cp $HLS_ROOT/vivado_design_$SCALE/vivado_design.runs/impl_1/design_1_wrapper.bit $HLS_ROOT/vivado_design_$SCALE/design_1.bit
